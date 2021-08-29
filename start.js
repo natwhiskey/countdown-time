4b3dbaed6d53558e04ef51f99595160c1204da0a
natwhiskey <kimngza@hotmail.com>
2021-08-29 22:40:14 +0700

    declare varible for set date-time

--- start.js
+++ start.js
@@ -1,5 +1,8 @@
 (() => {
     function countDown() {
+        const present = new Date().getTime();
+        const newYear = new Date('December 31, 2021 23:59:59').getTime();
+        const UnixTimeLeft = newYear - present;
     }
 
     function run() {
