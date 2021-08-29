2a647c3eaa222c80383647e5dc6520d3497bbed5
natwhiskey <kimngza@hotmail.com>
2021-08-29 22:41:45 +0700

    declare varible global

--- start.js
+++ start.js
@@ -1,4 +1,8 @@
 (() => {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
     function countDown() {
         const present = new Date().getTime();
         const newYear = new Date('December 31, 2021 23:59:59').getTime();
