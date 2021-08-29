72694edf7c7854ccb9fecc036b154921dcb42c18
natwhiskey <kimngza@hotmail.com>
2021-08-29 22:45:33 +0700

    commit

--- start.js
+++ start.js
@@ -3,6 +3,7 @@
     const minute = second * 60;
     const hour = minute * 60;
     const day = hour * 24;
+
     function countDown() {
         const present = new Date().getTime();
         const newYear = new Date('December 31, 2021 23:59:59').getTime();
