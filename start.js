c530d5c2a32533dc4d9ec92ec5d9b72fab8177b8
natwhiskey <kimngza@hotmail.com>
2021-08-29 22:45:16 +0700

    use innertext function for calculate

--- start.js
+++ start.js
@@ -7,6 +7,14 @@
         const present = new Date().getTime();
         const newYear = new Date('December 31, 2021 23:59:59').getTime();
         const UnixTimeLeft = newYear - present;
+
+        innerText('SECONDS', UnixTimeLeft % minute / second);
+        innerText('MINUTES', UnixTimeLeft % hour / minute);
+        innerText('HOURS', UnixTimeLeft % day / hour);
+        innerText('DAYS', UnixTimeLeft / day);
+
+    }
+
     function innerText(id, fomula) {
         const element = document.getElementById(id);
         element.innerText = Math.floor(fomula);
