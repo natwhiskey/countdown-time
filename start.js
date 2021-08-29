9bd9768ddf75434a81919ff3aebc635e77b765ef
natwhiskey <kimngza@hotmail.com>
2021-08-29 22:42:32 +0700

    create function innertext for get id in html file

--- start.js
+++ start.js
@@ -7,6 +7,9 @@
         const present = new Date().getTime();
         const newYear = new Date('December 31, 2021 23:59:59').getTime();
         const UnixTimeLeft = newYear - present;
+    function innerText(id, fomula) {
+        const element = document.getElementById(id);
+        element.innerText = Math.floor(fomula);
     }
 
     function run() {
